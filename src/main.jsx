ddb844a949ffe2cd674c334296a8b0b1ea406e8c
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2024-01-26 16:58:24 +0000

    Revert "updated main.jsx"
    This reverts commit d537aec19ae314db3a809d6e3124217e0bd21a57.

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,5 @@ import App from './App.jsx';
 import M from 'materialize-css';
 import "materialize-css/dist/css/materialize.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
